19e4994cf5753f7e3706c1a2432ce3f954e75ebb
Sigfrido Narváez <sigfrido@gmail.com>
2017-12-04 20:59:05 -0800

    Remove Device POJO

--- src/main/java/DeviceService.java
+++ src/main/java/DeviceService.java
@@ -5,17 +5,64 @@ import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.changestream.ChangeStreamDocument;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import java.util.Date;
+
+/**
+ Incoming data from devices
+ =======================
+ DeviceEvents (V1)
+ =======================
+ {
+ "_id" : ObjectId("5a261d8cbc54a05cb1e8a697"),
+ "deviceId" : 81,
+ "value" : 2410
+ }
+
+ =======================
+ DeviceEvents (V2)
+ =======================
+ {
+ "_id" : ObjectId("5a261e71bc54a05cb1ea8297"),
+ "deviceId" : 171,
+ "value" : 1684,
+ "location" : {
+     "type" : "Point",
+     "coordinates" : [
+         -87.33969,
+         38.44392
+     ]
+   }
+ }
+
+ Analyze and store
+ =======================
+ Devices
+ =======================
+ {
+ "_id" : 60,
+ "value" : 546
+ "date" : ISODate("2017-12-05T04:16:36.184Z"),
+ "isFirstGen" : true,
+ "isOutOfBounds" : true,
+ "location" : {
+     "type" : "Point",
+     "coordinates" : [
+         -87.33969,
+         38.44392
+     ]
+  }
+ }
+ */
 
 public class DeviceService {
 
-  private MongoCollection<Device> coll;
+  private MongoCollection<Document> coll;
   private MongoCollection<Document> collToWatch;
 
-  DeviceService(MongoCollection<Device> collection, MongoCollection<Document> collectionToWatch) {
+  DeviceService(MongoCollection<Document> collection, MongoCollection<Document> collectionToWatch) {
 
     this.coll = collection;
     this.collToWatch = collectionToWatch;
-
   }
 
   public void watchOutOfBounds() {
@@ -23,7 +70,6 @@ public class DeviceService {
     DeviceOutOfBoundsWatcher outOfBoundsWatcher = new DeviceOutOfBoundsWatcher(collToWatch, OutOfBoundsHandler);
 
     outOfBoundsWatcher.start();
-
   }
 
   public void watchFirstGen() {
@@ -31,7 +77,6 @@ public class DeviceService {
     DeviceFirstGenWatcher firstGenWatcher = new DeviceFirstGenWatcher(collToWatch, firstGenHandler);
 
     firstGenWatcher.start();
-
   }
 
   public Block<ChangeStreamDocument<Document>> OutOfBoundsHandler = new Block<ChangeStreamDocument<Document>>() {
@@ -39,24 +84,26 @@ public class DeviceService {
     @Override
     public void apply(final ChangeStreamDocument<Document> changeStreamDocument) {
 
-      Device device = new Device(changeStreamDocument.getFullDocument());
+      Document device = changeStreamDocument.getFullDocument();
 
-      System.out.println("DEVICE OUT OF BOUNDS: " + changeStreamDocument.getFullDocument().toString());
+      System.out.println("OUT OF BOUNDS: " + device.toString());
 
-      Bson filter = Filters.eq("_id", device.getdeviceId());
+      Bson filter = Filters.eq("_id", device.getInteger("deviceId")); //.getdeviceId());
 
       Bson update =  new Document("$set",
                                   new Document()
-                                    .append("_id", device.getdeviceId())
-                                    .append("value", device.getvalue())
-                                    .append("date", device.getDate()));
+                                    .append("_id", device.getInteger("deviceId"))
+                                    .append("value", device.getInteger("value"))
+                                    .append("isOutOfBounds", true)
+                                    .append("date", new Date()));
+
+      if (device.containsKey("location"))
+        ((Document)((Document) update).get("$set")).append("location", device.get("location"));
 
       UpdateOptions options = new UpdateOptions().upsert(true);
 
       coll.updateOne(filter, update, options);
-
     }
-
   };
 
 
@@ -65,25 +112,22 @@ public class DeviceService {
     @Override
     public void apply(final ChangeStreamDocument<Document> changeStreamDocument) {
 
-      Device device = new Device(changeStreamDocument.getFullDocument());
+      Document device = changeStreamDocument.getFullDocument();
 
-      System.out.println("FIRST GEN DEVICE: " + device.getdeviceId());
+      System.out.println("FIRST GEN: " + device.toString());
 
-      Bson filter = Filters.eq("_id", device.getdeviceId());
+      Bson filter = Filters.eq("_id", device.getInteger("deviceId"));
 
       Bson update =  new Document("$set",
               new Document()
-                      .append("_id", device.getdeviceId())
-                      .append("value", device.getvalue())
-                      .append("firstGen", true) // FLAG 1ST GEN DEVICES
-                      .append("date", device.getDate()));
+                      .append("_id", device.getInteger("deviceId"))
+                      .append("value", device.getInteger("value"))
+                      .append("isFirstGen", true)
+                      .append("date", new Date()));
 
       UpdateOptions options = new UpdateOptions().upsert(true);
 
       coll.updateOne(filter, update, options);
-
     }
-
   };
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,9 @@
 import com.mongodb.client.model.CreateCollectionOptions;
 import org.bson.Document;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class Main {
 
@@ -24,33 +18,25 @@ public class Main {
 
     MongoDatabase db = client.getDatabase("IoT");
 
-
     // Drop DeviceEvents collection and set to capped with max
     db.getCollection("DeviceEvents").drop();
 
     // Capped coll 1GB || 5,000 docs
     db.createCollection("DeviceEvents", new CreateCollectionOptions()
-                                                        .capped(true)
-                                                        .maxDocuments(5000)
-                                                        .sizeInBytes(1073741824));
+            .capped(true)
+            .maxDocuments(5000)
+            .sizeInBytes(1073741824));
 
     // Collections
     MongoCollection<Document> collDeviceEvents = db.getCollection("DeviceEvents");
+    MongoCollection<Document> collDevices = db.getCollection("Devices");
 
-    // Default POJO codec registry
-    CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
-            fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-
-    MongoCollection<Device> collDevices = db.getCollection("Devices", Device.class)
-            .withCodecRegistry(pojoCodecRegistry);
-
-
-    // Service and watch!
+    // Get Service and Watch!
     DeviceService deviceSvc = new DeviceService(collDevices, collDeviceEvents);
 
     deviceSvc.watchOutOfBounds();
 
     deviceSvc.watchFirstGen();
 
-    }
+  }
 }
